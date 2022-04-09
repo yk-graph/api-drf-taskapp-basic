2cd57ea3de308f423b5608442d8ae5b5ba732ac9
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-10 05:45:16 +0900

    feat: setting views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializers, UserSerializers
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = (AllowAny,)
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializers
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializers
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
